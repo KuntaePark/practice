ab3cfde9580796d63347fc50f550c485171e8689
KuntaePark <sgvelcro@naver.com>
2020-02-06 16:45:48 +0900

    3190 refac

--- 3190/3190.cpp
+++ 3190/3190.cpp
@@ -15,38 +15,50 @@ enum direction {
 };
 
 int map[MAX][MAX]; // 0:empty 1:body 2:apple
-int to[MAX][MAX];
+int connectTo[MAX][MAX];
 queue<tuple<int, char>> dirq;
-int head[2] = {0,0};
-int tail[2] = {0,0};
-int hd = RIGHT;
+
+int hx,hy;
+int ty,tx;
+int headDirection = RIGHT;
 int n = 0;
 int dy[4] = {-1,0,1,0};
 int dx[4] = {0,1,0,-1};
 
-bool move() {
-    int hy, hx, nhy, nhx;
-    int td = -1;
-    hy = head[0];
-    hx = head[1];
-    nhy = hy + dy[hd];
-    nhx = hx + dx[hd];
-    if(nhy < 0 || nhy >= n || nhx < 0 || nhx >= n) return false;
-    if(map[nhy][nhx] == 1) return false;
+void rotatehD(char rotatingDirection) {
+    if(rotatingDirection == 'D') headDirection = (headDirection+1) % 4;
+    else headDirection = (headDirection+3) % 4;
+}
 
-    head[0] = nhy;
-    head[1] = nhx;
-    to[hy][hx] = hd;
+bool isStop() {
+    int nhy,nhx;
+    nhy = hy + dy[headDirection];
+    nhx = hx + dx[headDirection];
+    if(nhy < 0 || nhy >= n || nhx < 0 || nhx >= n) return true;
+    if(map[nhy][nhx] == 1) return true;
+    else return false;
+
+}
+
+void move() {
+    int nhy, nhx;
+    int tailDirection;
+
+    nhy = hy + dy[headDirection];
+    nhx = hx + dx[headDirection];
+    hy = nhy;
+    hx = nhx;
+
+    connectTo[hy][hx] = headDirection;
+
     if(map[nhy][nhx] == 0) {
-        td = to[tail[0]][tail[1]];
-        map[tail[0]][tail[1]] = 0;
-        to[tail[0]][tail[1]] = -1;
-        tail[0] += dy[td];
-        tail[1] += dx[td];
+        tailDirection = connectTo[ty][tx];
+        map[ty][tx] = 0;
+        connectTo[ty][tx] = -1;
+        ty += dy[tailDirection];
+        tx += dx[tailDirection];
     }
     map[nhy][nhx] = 1; 
-    return true;
-
 }
 
 int main() {
@@ -54,13 +66,18 @@ int main() {
     int ax, ay;
     int count = 0;
     char c;
-    tuple<int, char> cur;
+    tuple<int, char> nextChange;
+
+    hy = 0;
+    hx = 0;
+    ty = 0;
+    tx = 0;
 
     for(int i = 0; i < MAX; i++) for(int j = 0; j < MAX; j++) map[i][j] = 0;
-    for(int i = 0; i < MAX; i++) for(int j = 0; j < MAX; j++) to[i][j] = -1;
+    for(int i = 0; i < MAX; i++) for(int j = 0; j < MAX; j++) connectTo[i][j] = -1;
 
     map[0][0] = 1;
-    to[0][0] = RIGHT;
+    connectTo[0][0] = RIGHT;
 
     scanf("%d", &n);
     scanf("%d", &k);
@@ -75,18 +92,16 @@ int main() {
         dirq.push(make_tuple(x,c));
     }
 
-    while(1) {
+    while(!isStop()) {
         count++;
-        if(!move()) break;
-        cur = dirq.front();
-        if(count == get<0>(cur)) {
-            if(get<1>(cur) == 'D') hd =(hd+1) % 4;
-            else hd = (hd+3) % 4;
-            to[head[0]][head[1]] = hd;
+        move();
+        nextChange = dirq.front();
+        if(count == get<0>(nextChange)) {
+            rotatehD(get<1>(nextChange));
+            connectTo[hy][hx] = headDirection;
             dirq.pop();
         }
-
     }
 
-    std::cout<<count;
+    std::cout<<count+1;
 }
